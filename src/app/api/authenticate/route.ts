781fd85bf4baf82cd1892998b9b3e890012c19a5
Marcus N Ahlin <marcusnahlin@gmail.com>
2024-09-19 16:51:40 +0200

    Added correct password comparing with hashing

--- src/app/api/authenticate/route.ts
+++ src/app/api/authenticate/route.ts
@@ -1,5 +1,6 @@
-import { NextRequest, NextResponse } from "next/server";
+import { NextRequest, NextResponse, userAgent } from "next/server";
 import { PrismaClient } from "@prisma/client";
+const bcrypt = require("bcrypt");
 
 async function authenticate(req: NextRequest, res: NextResponse) {
     const prisma = new PrismaClient();
@@ -20,14 +21,19 @@ async function authenticate(req: NextRequest, res: NextResponse) {
         return NextResponse.json({ message: "No account found!", error: true, status: 401, ok: false });
     }
 
-    const hashedPassword = await prisma.user.findUnique({
-        where: { id: authenticatedUser.id },
-        select: { hash_pass: true },
-    });
+    try {
+        const userVerified = await prisma.user.findUnique({
+            where: { id: authenticatedUser.id },
+            select: { hash_pass: true },
+        });
 
-    if (hashedPassword?.hash_pass === password) {
-        authenticated = true;
+        if (userVerified?.hash_pass) {
+            authenticated = await bcrypt.compare(password, userVerified.hash_pass);
+        }
+    } catch (e) {
+        return NextResponse.json({ message: "Authentication failed!", error: true, status: 401, ok: false });
     }
+
 
     if (authenticated) {
         return NextResponse.json({ message: "Authenticated!", error: false, status: 200, ok: true });
