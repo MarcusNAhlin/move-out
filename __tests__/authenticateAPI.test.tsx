6d40c1478927960f9fb4f0f060d10c0d9974aecf
Marcus N Ahlin <marcusnahlin@gmail.com>
2024-09-23 14:33:16 +0200

    Added tests for missing input

---
+++ __tests__/authenticateAPI.test.tsx
@@ -0,0 +1,35 @@
+import { NextRequest, NextResponse } from 'next/server';
+import { POST } from '../src/app/api/authenticate/route';
+
+// Mock NextResponse
+jest.mock('next/server', () => ({
+    NextResponse: {
+        json: jest.fn().mockImplementation((data) => ({
+            ...data,
+            status: 200,
+        })),
+    },
+}));
+
+describe('authenticate function', () => {
+    beforeEach(() => {
+        jest.clearAllMocks();
+    });
+
+    it('returns an error if email or password is missing', async () => {
+        const req = {
+            json: jest.fn().mockResolvedValue({}),
+        } as unknown as NextRequest;
+
+        const res = {} as NextResponse;
+
+        const response = await POST(req, res);
+
+        expect(NextResponse.json).toHaveBeenCalledWith({
+            message: "Missing email or password!",
+            error: true,
+            status: 401,
+            ok: false,
+        });
+    });
+});
