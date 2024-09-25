4963b776526a1a8d1691c48ba3bf64ab6f7d8ba1
Marcus N Ahlin <marcusnahlin@gmail.com>
2024-09-25 11:32:33 +0200

    Added test for rendering header

---
+++ __tests__/labelComponent.test.tsx
@@ -0,0 +1,41 @@
+import '@testing-library/jest-dom';
+import { render, screen } from '@testing-library/react';
+import { MantineProvider } from '@mantine/core';
+import { useSession } from 'next-auth/react';
+import LabelComponent from '@/components/Label';
+
+// Mock the `useSession` hook from next-auth/react
+jest.mock('next-auth/react');
+
+describe('Label Page', () => {
+    it('renders label header', async () => {
+        // Mock session data
+        (useSession as jest.Mock).mockReturnValue({
+            data: {
+                user: {
+                    email: "test@gmail.com"
+                }
+            },
+            status: 'authenticated',
+        });
+
+        const label = {
+            id: "123",
+            title: "plates",
+            type: "NORMAL",
+        }
+
+        // Render the Home component
+        render(
+            <MantineProvider>
+                <LabelComponent label={label} printBtn />
+            </MantineProvider>
+        );
+
+        const labelText = "plates";
+
+        // Assert
+        expect(await screen.getByRole('heading', { level: 3 }));
+        expect(await screen.getByText(labelText)).toBeInTheDocument();
+    });
+});
