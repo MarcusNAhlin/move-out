64f69141ced184265b44db7bf43d39d57dd4e9fc
Marcus N Ahlin <marcusnahlin@gmail.com>
2024-09-25 13:25:54 +0200

    Added test for adding label (database)

--- __tests__/labelAddAPI.test.tsx
+++ __tests__/labelAddAPI.test.tsx
@@ -1,6 +1,9 @@
 import { NextRequest, NextResponse } from 'next/server';
 import { POST } from '../src/app/api/label/add/route';
-import label from '@/components/Label';
+import { prismaMock } from '../singleton';
+import { addLabel } from '@/database_calls/labels';
+
+type LabelType = "NORMAL" | "FRAGILE" | "HAZARDOUS";
 
 // Mock NextResponse
 jest.mock('next/server', () => ({
@@ -75,5 +78,57 @@ describe('Label add API', () => {
             status: 401,
             ok: false,
         });
+    });
+
+    it('returns success when creating label', async () => {
+        const req = {
+            json: jest.fn().mockResolvedValue({
+                email: "test@gmail.com",
+                labelTitle: "test",
+                labelDesign: "NORMAL"
+            }),
+        } as unknown as NextRequest;
+
+        const createdAt = new Date();
+        const updatedAt = new Date();
+
+        const label = {
+            id: "291b8587-314c-4ab7-ac17-76fb9048c328",
+            userId: 1,
+            title: "test label",
+            text: "",
+            type: "NORMAL" as LabelType,
+            picturePath: "",
+            soundPath: "",
+            createdAt: createdAt,
+            updatedAt: updatedAt
+        };
+
+        prismaMock.label.create.mockResolvedValue(label);
+
+        await expect(addLabel(label)).resolves.toEqual({
+            id: "291b8587-314c-4ab7-ac17-76fb9048c328",
+            userId: 1,
+            title: "test label",
+            text: "",
+            type: "NORMAL" as LabelType,
+            picturePath: "",
+            soundPath: "",
+            createdAt: createdAt,
+            updatedAt: updatedAt
+        });
+
+        // const res = {} as NextResponse;
+
+        // const response = await POST(req);
+
+
+
+        // expect(NextResponse.json).toHaveBeenCalledWith({
+        //     message: "No label design provided!",
+        //     error: true,
+        //     status: 401,
+        //     ok: false,
+        // });
     });
 });
