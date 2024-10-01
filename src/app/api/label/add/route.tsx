2b44f6d5d007ebc03b43211df1d9f83b8551839f
Marcus N Ahlin <marcusnahlin@gmail.com>
2024-10-01 18:58:42 +0200

    Added image upload functionality

--- src/app/api/label/add/route.tsx
+++ src/app/api/label/add/route.tsx
@@ -2,22 +2,52 @@ import { NextRequest, NextResponse } from "next/server";
 import { PrismaClient } from "@prisma/client";
 import { addLabel as addLabelToDB } from "@/database_calls/labels";
 import { LabelType } from "@/lib/types";
+import fs from 'fs';
+import path from "path";
+
+interface Image {
+    size: number;
+    type: string;
+    name: string;
+    lastModified: number;
+}
+
+export const config = {
+    api: {
+      bodyParser: false,
+    },
+  };
 
 async function addLabel(req: NextRequest) {
     const prisma = new PrismaClient();
-    const { labelTitle, labelDesign, labelTextContent, email }:
-        { labelTitle: string, labelDesign: string, labelTextContent:string,  email: string } = await req.json();
+    // const { labelTitle, labelDesign, labelTextContent, email, labelImage }:
+    //     { labelTitle: string, labelDesign: string, labelTextContent:string,  email: string, labelImage: any } = await req.json();
+
+    const formData = await req.formData();
+    const image: any | null = formData.get("labelImage") as any;
+    const labelTitle = formData.get("labelTitle") as string;
+    const labelDesign = formData.get("labelDesign") as string;
+    const labelTextContent = formData.get("labelTextContent") as string;
+    const email = formData.get("email") as string;
+
+    console.log(formData);
+
+    if (image) {
+        if (image.size > 10000000) {
+            return NextResponse.json({ message: "Image too big!", error: true, status: 401, ok: false }, { status: 401 });
+        }
+    }
 
     if (!labelTitle) {
-        return NextResponse.json({ message: "No label title provided!", error: true, status: 401, ok: false });
+        return NextResponse.json({ message: "No label title provided!", error: true, status: 401, ok: false }, { status: 401 });
     }
 
     if (!labelDesign) {
-        return NextResponse.json({ message: "No label design provided!", error: true, status: 401, ok: false });
+        return NextResponse.json({ message: "No label design provided!", error: true, status: 401, ok: false }, { status: 401 });
     }
 
     if (!email) {
-        return NextResponse.json({ message: "No email provided!", error: true, status: 401, ok: false });
+        return NextResponse.json({ message: "No email provided!", error: true, status: 401, ok: false }, { status: 401 });
     }
 
     let userId: number | null = null;
@@ -38,8 +68,10 @@ async function addLabel(req: NextRequest) {
 
         userId = user.id;
     } catch (e) {
-        return NextResponse.json({ message: "User not found!", error: true, status: 401, ok: false });
+        return NextResponse.json({ message: "User not found!", error: true, status: 401, ok: false }, { status: 401 });
     }
+
+    let imagePath: string | null = null;
 
     try {
         // TODO: Fix type!
@@ -48,16 +80,36 @@ async function addLabel(req: NextRequest) {
             title: labelTitle,
             type: labelDesign as LabelType,
             text: labelTextContent || "",
+            picturePath: imagePath || null
         }
 
-        await addLabelToDB(label)
+        const newLabel = await addLabelToDB(label)
 
-        return NextResponse.json({ message: "Label created!", error: false, status: 200, ok: true });
+        if (image) {
+            imagePath = path.join(process.cwd(), "public", "images", "user-images", `${userId}`, `${newLabel.id}`, `${image.name}`);
+
+            try {
+                // Convert image to buffer
+                const bytes = await image.arrayBuffer();
+                const buffer = Buffer.from(bytes);
+
+                // Create directory if it doesn't exist
+                await fs.mkdirSync(path.join(process.cwd(), "public", "images", "user-images", `${userId}`, `${newLabel.id}`), { recursive: true });
+
+                // Save image
+                await fs.writeFileSync(imagePath, buffer);
+            } catch (e) {
+                console.log(e);
+                return NextResponse.json({ message: "Error saving image!", error: true, status: 401, ok: false }, { status: 401 });
+            }
+        }
+
+        return NextResponse.json({ message: "Label created!", error: false, status: 200, ok: true }, { status: 200 });
     } catch (e) {
-        return NextResponse.json({ message: "Error creating label!", error: true, status: 401, ok: false });
+        return NextResponse.json({ message: "Error creating label!", error: true, status: 401, ok: false }, { status: 401 });
     }
 
-    return NextResponse.json({ message: "Error creating label!", error: true, status: 401, ok: false });
+    return NextResponse.json({ message: "Error creating label!", error: true, status: 401, ok: false }, { status: 401 });
 }
 
 export { addLabel as POST };
--- src/app/label/add/page.tsx
+++ src/app/label/add/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { Alert, Button, Group, Select, TextInput, Textarea } from "@mantine/core";
+import { Alert, Button, FileInput, Group, Select, TextInput, Textarea } from "@mantine/core";
 import { useForm } from "@mantine/form";
 import { useSession } from "next-auth/react";
 import { useRouter } from "next/navigation";
@@ -14,18 +14,21 @@ export default function LabelAddPage() {
 
     const form = useForm({
         mode: 'uncontrolled',
-        initialValues: { labelTitle: '', labelDesign: '', labelTextContent: '' },
+        initialValues: { labelTitle: '', labelDesign: '', labelTextContent: '', labelImage: null },
 
         validate: {
             labelTitle: (value: string) => (value.length < 0 ? 'Title too short' :
                                 value.length > 50 ? 'Title too long' : null),
+            labelImage: (value: any) => (value && value.size > 10000000 ? 'Image too big, max 10 MB' : null),
         },
     });
 
-    async function handleFormSubmit() {
+    async function handleFormSubmit(values: any) {
         setAddingLabel(true);
 
-        const { labelTitle, labelDesign, labelTextContent } = form.getValues();
+        // const { labelTitle, labelDesign, labelTextContent, labelImage } = form.getValues();
+
+
 
         if (!session?.user?.email) {
             setMessage("You need to be logged in to add labels");
@@ -33,27 +36,44 @@ export default function LabelAddPage() {
             return;
         }
 
+        const formData = new FormData();
+        formData.append('email', session.user.email);
+        formData.append('labelTitle', values.labelTitle);
+        formData.append('labelDesign', values.labelDesign);
+        formData.append('labelTextContent', values.labelTextContent);
+        if (values.labelImage) {
+            formData.append('labelImage', values.labelImage);
+        }
+
         try {
+            // const response: any = await fetch(`/api/label/add`, {
+            //     method: 'POST',
+            //     headers: {
+            //         'Content-Type': 'application/json',
+            //     },
+            //     body: JSON.stringify({
+            //         labelTitle: labelTitle,
+            //         labelDesign: labelDesign,
+            //         labelTextContent: labelTextContent,
+            //         labelImage: labelImage,
+            //         email: session.user.email,
+            //     }),
+            // });
             const response: any = await fetch(`/api/label/add`, {
                 method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json',
-                },
-                body: JSON.stringify({
-                    labelTitle: labelTitle,
-                    labelDesign: labelDesign,
-                    labelTextContent: labelTextContent,
-                    email: session.user.email,
-                }),
+                body: formData,
+                cache: 'no-store'
             });
+
+            console.log(response);
+
+            if (!response.ok) {
+                throw new Error(response.error);
+            }
 
             if (!response?.error) {
                 router.push("/");
                 router.refresh();
-            }
-
-            if (!response.ok) {
-                throw new Error(response.error);
             }
         } catch (e: any) {
             setMessage(e.message);
@@ -95,6 +115,13 @@ export default function LabelAddPage() {
                         autosize
                         key={form.key('labelTextContent')}
                         {...form.getInputProps('labelTextContent')}
+                    />
+                    <FileInput
+                        size="md"
+                        label="Image"
+                        placeholder="Click to add image"
+                        key={form.key('labelImage')}
+                        {...form.getInputProps('labelImage')}
                     />
                     {
                         message ? <Alert variant="light" color="red">
