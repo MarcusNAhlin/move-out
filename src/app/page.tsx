ed0c021ec939314b811fea3161e21a00864fb1c5
Marcus N Ahlin <marcusnahlin@gmail.com>
2024-10-10 10:21:41 +0200

    Fetching user from DB

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,9 +2,43 @@
 import { Button, Flex, Skeleton, Text, Title } from "@mantine/core";
 import { signIn, useSession } from "next-auth/react";
 import LabelHolder from "@/components/LabelHolder";
+import { useEffect, useState } from "react";
+import { User } from "@/lib/types";
 
 export default function Home() {
   const { data: session, status } = useSession();
+  const [user, setUser] = useState<User | null>(null);
+
+  useEffect(() => {
+    async function getUser() {
+      try {
+        const user = await fetch(`/api/account/getUser?email=${session?.user?.email}`, {
+          method: "GET",
+          headers: {
+            "Content-Type": "application/json",
+          }});
+
+        const data = await user.json();
+
+        if (data.ok) {
+          setUser(data.user);
+
+          return user;
+        }
+
+        if (!data.ok) {
+          throw new Error(data.message)
+        }
+      } catch (e) {
+        return console.error(e);
+      }
+    }
+
+    if (!user) {
+      getUser();
+    }
+
+  }, [session]);
 
   return (
     <>
@@ -33,10 +67,23 @@ export default function Home() {
               {status === "authenticated" ? <Text>Welcome, {session.user?.email}</Text> : null }
               {status === "unauthenticated" ? <Text>You are not logged in</Text> : null }
             {
+              user?.role === "ADMIN" && <>
+                <Button
+                  component="a"
+                  href="/admin-panel"
+                  m={"sm"}
+                  mt={"sm"}
+                  w={"8rem"}
+                  color="orange"
+                >Admin Panel</Button>
+              </>
+            }
+            {
               status === "authenticated" ?
               <Button
                 component="a"
                 href="/profile"
+                m={"sm"}
                 mt={"sm"}
                 w={"8rem"}
               >Visit Profile</Button> : null
