0dd32f83e95ba17374b1e7f73b4f85a96364c84c
Marcus N Ahlin <marcusnahlin@gmail.com>
2024-10-24 12:14:29 +0200

    Removed test for not logged in, now redirected instead

--- __tests__/frontend/landingPage.test.tsx
+++ __tests__/frontend/landingPage.test.tsx
@@ -8,67 +8,6 @@ import Home from '@/app/page';
 jest.mock('next-auth/react');
 
 describe('Home Page', () => {
-    it('renders an h1 header', () => {
-        // Mock session data
-        (useSession as jest.Mock).mockReturnValue({
-            data: null,
-            status: 'unauthenticated',
-        });
-
-        // Render the Home component
-        render(
-            <MantineProvider>
-                <Home />
-            </MantineProvider>
-        );
-
-        const header = screen.getByRole('heading', { level: 1 });
-
-        // Assert
-        expect(header);
-    });
-
-    it('renders "not logged in" if not', async () => {
-        // Mock session data
-        (useSession as jest.Mock).mockReturnValue({
-            data: null,
-            status: 'unauthenticated',
-        });
-
-        // Render the Home component
-        render(
-            <MantineProvider>
-                <Home />
-            </MantineProvider>
-        );
-
-        const notSignedInText = "You are not logged in";
-
-        // Assert
-        expect(await screen.getByText(notSignedInText)).toBeInTheDocument();
-    });
-
-    it('renders login button if not signed in', async () => {
-        // Mock session data
-        (useSession as jest.Mock).mockReturnValue({
-            data: null,
-            status: 'unauthenticated',
-        });
-
-        // Render the Home component
-        render(
-            <MantineProvider>
-                <Home />
-            </MantineProvider>
-        );
-
-        const buttonText = "Sign In";
-
-        // Assert
-        expect(await screen.getByRole("button")).toBeInTheDocument();
-        expect(await screen.getByText(buttonText)).toBeInTheDocument();
-    });
-
     it('renders email if signed in', async () => {
         // Mock session data
         (useSession as jest.Mock).mockReturnValue({
